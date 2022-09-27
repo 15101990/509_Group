4c9640da8048656bf644ccd7b4a6e8dba9e3dd88
15101990 <m.helmy.amit@gmail.com>
2022-09-27 20:55:58 +0200

    khkhkhk

--- HAL/AT24C16A/AT24C16A_Prog.c
+++ HAL/AT24C16A/AT24C16A_Prog.c
@@ -14,16 +14,17 @@ void H_AT24C16A_Void_EEPROMInit(void)
 }
 void H_AT24C16A_Void_EEPROMWrite(u8 Copy_U8_Page,u8 Copy_U8_Byte,u8 Copy_U8_Data)
 {
-	M_I2C_Void_I2CStartCondition();
-	M_I2C_Void_I2CSendSlaveAddressWrite(0x50 | Copy_U8_Page);
-	M_I2C_Void_I2CSendByte(Copy_U8_Byte);
-	M_I2C_Void_I2CSendByte(Copy_U8_Data);
-	M_I2C_Void_I2CStopCondition();
+
 }
 u8   H_AT24C16A_Void_EEPROMRead(u8 Copy_U8_Page,u8 Copy_U8_Byte)
 {
 	u8 Local_U8_Reading = 0;
 	M_I2C_Void_I2CStartCondition();
+        M_I2C_Void_I2CStartCondition();
+        M_I2C_Void_I2CStartCondition();
+        M_I2C_Void_I2CStartCondition();
+        M_I2C_Void_I2CStartCondition();
+        M_I2C_Void_I2CStartCondition();
 	M_I2C_Void_I2CSendSlaveAddressWrite(0x50 | Copy_U8_Page);
 	M_I2C_Void_I2CSendByte(Copy_U8_Byte);
 	M_I2C_Void_I2CRepeatedStart();
